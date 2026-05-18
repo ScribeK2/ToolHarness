b08d83f016a6fafbb052f16f0891aeb0e6ccc1bd
ScribeK2 <aweiward@protonmail.com>
2026-05-18 19:46:00 -0400

    test(update_checker): remove redundant banner_data appimage_url test

--- test/services/update_checker_test.rb
+++ test/services/update_checker_test.rb
@@ -75,17 +75,6 @@ class UpdateCheckerTest < ActiveSupport::TestCase
     assert_equal "https://example.com/TH.AppImage", cached["appimage_url"]
   end
 
-  test "banner_data exposes appimage_url" do
-    Rails.cache.write(UpdateChecker::CACHE_KEY, {
-      "tag_name"     => "v99.0.0",
-      "html_url"     => "https://github.com/ScribeK2/ToolHarness/releases/tag/v99.0.0",
-      "appimage_url" => "https://example.com/TH.AppImage"
-    }, expires_in: 24.hours)
-
-    data = UpdateChecker.banner_data
-    assert_equal "https://example.com/TH.AppImage", data[:appimage_url]
-  end
-
   test "refresh! caches nil appimage_url when no .AppImage asset is present" do
     response_body = {
       "tag_name" => "v99.0.0",
