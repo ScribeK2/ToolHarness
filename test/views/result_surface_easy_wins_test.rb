1f3a2a5d0d0ac693c5a81b6ba2ace02c9c37291c
ScribeK2 <aweiward@protonmail.com>
2026-05-29 05:06:13 -0400

    feat(results): send-target-to-another-tool handoff menu

--- test/views/result_surface_easy_wins_test.rb
+++ test/views/result_surface_easy_wins_test.rb
@@ -28,4 +28,17 @@ class ResultSurfaceEasyWinsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "form[action='#{tool_run_rerun_path(run)}'][method=post] button", text: /re-run/i
   end
+
+  test "result header offers a handoff menu to sibling tools with target prefilled" do
+    run = completed_run # dns_lookup, domain example.com
+    get workbench_path(tool: "dns_lookup", target: "example.com", run: run.id)
+    assert_response :success
+
+    assert_select "details" do
+      assert_select "a[href=?]", workbench_path(tool: "whois_lookup", target: "example.com")
+      assert_select "a[href=?]", workbench_path(tool: "ping", target: "example.com")
+    end
+    # never links back to the current tool
+    assert_select "details a[href*='tool=dns_lookup']", false
+  end
 end
